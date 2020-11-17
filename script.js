33a703f36f031747538318e4f9e5037d01376ed2
pplexed <63002801+pplexed@users.noreply.github.com>
2020-11-16 19:14:09 -0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ canvas.width = window.innerWidth;
 
 const images = {};
 images.player = new Image();
-images.player.src = './img/Cuphead.png';
+images.player.src = 'https://i.ibb.co/Ybk7y06/character.png';
 const characterActions = ['up', 'top right', 'right', 'down right', 'down', 'jump'];
 const characters = [];
 
